65ae73b024c287512199998e415c0cef06974b50
RehamanSyed <Rehaman.syed1990@gmail.com>
2023-06-01 02:27:16 +0530

    global db

--- connection/db.js
+++ connection/db.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 
+
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGOOSE_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("connection connected", conn.connection.host);
+    console.log("connection connected==>", conn.connection.host);
+
   } catch (error) {
     console.log("error in db", error.message);
     process.exit();
